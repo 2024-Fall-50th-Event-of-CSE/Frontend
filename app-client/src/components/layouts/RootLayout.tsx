2b7f54569de5c4a1aafc17e6e4a308f003c1b117
toothlessdev <toothless042@gmail.com>
2024-11-08 22:57:47 +0900

    style: remove debug layout border

--- app-client/src/components/layouts/RootLayout.tsx
+++ app-client/src/components/layouts/RootLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function RootLayout() {
     return (
-        <main className="w-full h-screen max-w-[600px] p-[10px] mx-auto border-[1px] border-red-500">
+        <main className="w-full h-screen max-w-[600px] p-[10px] mx-auto ">
             <Outlet />
         </main>
     );
