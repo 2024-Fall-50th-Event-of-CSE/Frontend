e8f762c8568b0c28d1a903026f46565888155bc9
toothlessdev <toothless042@gmail.com>
2024-11-08 22:51:24 +0900

    chore: re-assign file paths

--- app-client/src/main.tsx
+++ app-client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./apps/App.tsx";
+import "@/apps/index.css";
+import App from "@/apps/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
