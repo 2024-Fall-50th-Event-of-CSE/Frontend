6ad86d813690d60ad3c73aa35ebcde99cdc322c1
toothlessdev <toothless042@gmail.com>
2024-11-08 22:52:41 +0900

    feature: add StampCard component

---
+++ app-client/src/components/displays/StampCard.tsx
@@ -0,0 +1,32 @@
+import { Card, CardContent } from "@/components/ui/card";
+
+import logoStamp from "@/assets/logo.webp";
+
+export interface StampCardProps {
+    icon?: React.ReactNode;
+    imgSrc?: string;
+    name: string;
+    description?: string;
+    active?: boolean;
+}
+
+export const StampCard = ({ icon, name, description, active = true }: StampCardProps) => {
+    return (
+        <Card
+            className={`h-[80px] border-[2px] flex m-1 px-2 items-center justify-center hover:cursor-pointer relative`}
+        >
+            <div className="">{icon}</div>
+            <CardContent className="m-2 p-0 w-full">
+                <h2 className="text-xl font-bold">{name}</h2>
+                {description && <p>{description}</p>}
+                {active && (
+                    <img
+                        src={logoStamp}
+                        alt=""
+                        className="w-[50px] aspect-square rotate-[30deg] absolute top-1 right-2"
+                    />
+                )}
+            </CardContent>
+        </Card>
+    );
+};
