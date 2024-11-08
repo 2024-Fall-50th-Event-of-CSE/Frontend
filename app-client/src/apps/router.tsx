fa1ed5c8414a99053a66944281c53a352814ea5c
toothlessdev <toothless042@gmail.com>
2024-11-08 22:51:39 +0900

    feature: add routes for each pages

--- app-client/src/apps/router.tsx
+++ app-client/src/apps/router.tsx
@@ -1,11 +1,26 @@
 import RootLayout from "@/components/layouts/RootLayout";
+import AuthPage from "@/pages/AuthPage";
+import CertificateCashPage from "@/pages/CertificatePage/CashPage";
+import CertificateQuizPage from "@/pages/CertificatePage/QuizPage";
+import CertificateSocialPage from "@/pages/CertificatePage/SocialPage";
 import HomePage from "@/pages/HomePage";
+import MyPage from "@/pages/MyPage";
+import RegisterPage from "@/pages/RegisterPage";
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<RootLayout />}>
             <Route path="/" element={<HomePage />} />
+            <Route path="register" element={<RegisterPage />} />
+            <Route path="login" element={<AuthPage />} />
+            <Route path="mypage" element={<MyPage />} />
+
+            <Route path="certificate">
+                <Route path="social" element={<CertificateSocialPage />} />
+                <Route path="quiz" element={<CertificateQuizPage />} />
+                <Route path="cash" element={<CertificateCashPage />} />
+            </Route>
         </Route>
     )
 );
